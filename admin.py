cc397386f26944cb255d2ef7b64fdae5a9aedf34
Jude Sajith <judesajith.aj@gmail.com>
2023-05-10 14:00:46 +0530

    ssss

--- admin.py
+++ admin.py
@@ -5,16 +5,22 @@ import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def call_openai(source):
-    messages=[{"role": "system", "content": f"You are the Buyer persona represents the following {source} given this information answer the following questions :\n What is this buyer persona’s most important job responsibilities and activities? • What are the top 5 obstacles that would interfere with this buyer persona’s career success? Where would this buyer persona look for information about products like BrandBuilder and other industry trends? • What role with this buyer persona play in the purchase process, would they define the need, evaluate different solutions, make the final purchase decision or some combination of those roles? • What other roles in the company would likely be part of the buying process and decision-making? • Why wouldn’t this buyer persona already have a product like Brandbuilder? • Are there related products that this buyer’s team uses that would need to integrate with Brandbuilder? • What concerns would this buyer have that might keep them from making the purchase decision? \n the response should be a valid json list with the questions and the answers "}]
-    # if "messages" not in st.session_state:
-    #     st.session_state.messages = [{"role": "user", "content": f"You are the Buyer persona represents the following {} "}]
-    # st.session_state.messages.append(message)
+def call_openai(source,initprompt):
+    messages={"role": "system", "content": source}
+    if "messages" not in st.session_state:
+        st.session_state.messages = [{"role": "user", "content": f"you are an interviewer who will ask 7 questions, (only one question at a time ), given in the following json list with sample answers :{initprompt}\n, if the users answer misses any information from the actual , then ask an additional question in a way that you hint the user to provide the missing information in the next answer before moving to the next questions "}]
+        response = openai.ChatCompletion.create(
+                                                    model="gpt-4-0314",
+                                                    max_tokens=7000,
+                                                    temperature=0.7,
+                                                    messages = messages
+                                                )
+    st.session_state.messages.append(messages)
     response = openai.ChatCompletion.create(
         model="gpt-4-0314",
         max_tokens=7000,
         temperature=0.7,
-        messages = messages
+        messages = st.session_state.messages
     )
     return response.choices[0].message['content']
 
@@ -42,13 +48,15 @@ def main():
     if st.button("Generate Answers"):
         response = call_openai(promp)
         jsonstr = json.loads(response)
-        st.write(jsonstr)
+        if "jsonstr" not in st.session_state:
+            st.session_state.jsonstr = jsonstr
+        st.write(st.session_state.jsonstr)
     conversation = st.empty()
 
     user_input = st.text_input("You:")
 
     if st.button("Send"):
-        bot_response = call_openaiturbo(user_input)
+        bot_response = call_openaiturbo(user_input,st.session_state.jsonstr)
 
         conversation.markdown(f'**You:** {user_input}')
         conversation.markdown(f'**Bot:** {bot_response}')
