e18a3d27b0939522da19f12dd7f65ffd2780ebf7
Jude Sajith <judesajith.aj@gmail.com>
2023-05-10 15:23:35 +0530

    sss

--- admin.py
+++ admin.py
@@ -7,7 +7,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def call_openai(source):
     # if "messages" not in st.session_state:
-    messages=[{"role": "system", "content": f"You are the Buyer persona represents the following {source} given this information answer the following questions :\n What is this buyer persona’s most important job responsibilities and activities? • What are the top 5 obstacles that would interfere with this buyer persona’s career success? Where would this buyer persona look for information about products like BrandBuilder and other industry trends? • What role with this buyer persona play in the purchase process, would they define the need, evaluate different solutions, make the final purchase decision or some combination of those roles? • What other roles in the company would likely be part of the buying process and decision-making? • Why wouldn’t this buyer persona already have a product like Brandbuilder? • Are there related products that this buyer’s team uses that would need to integrate with Brandbuilder? • What concerns would this buyer have that might keep them from making the purchase decision? \n the response should consist of the questions and the answers "}]
+    messages=[{"role": "system", "content": f"You are the Buyer persona represents the following {source} given this information answer the following questions :\n What is your most important job responsibilities and activities? • What are the top 5 obstacles that would interfere with your career success? Where would you look for information about products like your company and other industry trends? • What role do you play in the purchase process, would they define the need, evaluate different solutions, make the final purchase decision or some combination of those roles? • What other roles in the company would likely be part of the buying process and decision-making? • Why wouldn’t you already have a product like yours? • Are there related products that this buyer’s team uses that would need to integrate with your product? • What concerns would you have that might keep them from making the purchase decision? \n the response should consist of the questions and the answers "}]
 
     # messages = [ {"role": "user", "content": f"You are the Buyer persona represents the following {source} "}]
     # st.session_state.messages.append(message)
@@ -23,7 +23,7 @@ def call_openai(source):
 def call_openaiturbo(inpt,source):
     
     if "messages" not in st.session_state:
-        st.session_state.messages= [{"role": "user", "content": "you are an interviewer who will ask 7 questions about the user, (only one question at a time ), given in the following list with sample answers :\n"+str(source)+" \n, if the users answers contain similar information from the actual answer but still misses any information, then ask an additional follow up question that obtains a binary answer about the missing information, at the end of all questions create a buyer persona for the user who responded "}]
+        st.session_state.messages= [{"role": "user", "content": "you are an interviewer who will ask 7 questions about the user, (only one question at a time ), given in the following list with sample answers :\n"+str(source)+" \n, if the users answers contain similar information from the actual answer and misses any information, then ask an additional follow up question that obtains a binary answer about the missing information, at the end of all questions create a buyer persona for the user who responded "}]
     message={"role": "user", "content": str(inpt)}
     st.session_state.messages.append(message)
     response = openai.ChatCompletion.create(
@@ -55,7 +55,7 @@ def main():
         bot_response = call_openaiturbo(user_input,str(st.session_state.response))
 
         conversation.markdown(f'**You:** {user_input}')
-        conversation.markdown(f'**Bot:** {bot_response}')
+        conversation.info(f'**Bot:** {bot_response}')
 
 if __name__ == "__main__":
     main()
