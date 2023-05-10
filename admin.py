89081d523b892c99676c4288f2402d03d90a86c3
Jude Sajith <judesajith.aj@gmail.com>
2023-05-10 13:35:51 +0530

    ssss

--- admin.py
+++ admin.py
@@ -5,15 +5,15 @@ import openai
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def call_openai(source):
-    message={"role": "user", "content": source}
-    if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "user", "content": "You are a biology quiz bot that asks me questions and verify my answer and keep iterating till I say stop, Ask me 10 questions about biology one after the other and correct my responses before moving to the next question"}]
-    st.session_state.messages.append(message)
+    messages=[{"role": "user", "content": f"You are the Buyer persona represents the following {source} given this information answer the following questions :\n What is this buyer persona’s most important job responsibilities and activities? • What are the top 5 obstacles that would interfere with this buyer persona’s career success? Where would this buyer persona look for information about products like BrandBuilder and other industry trends? • What role with this buyer persona play in the purchase process, would they define the need, evaluate different solutions, make the final purchase decision or some combination of those roles? • What other roles in the company would likely be part of the buying process and decision-making? • Why wouldn’t this buyer persona already have a product like Brandbuilder? • Are there related products that this buyer’s team uses that would need to integrate with Brandbuilder? • What concerns would this buyer have that might keep them from making the purchase decision?   "}]
+    # if "messages" not in st.session_state:
+    #     st.session_state.messages = [{"role": "user", "content": f"You are the Buyer persona represents the following {} "}]
+    # st.session_state.messages.append(message)
     response = openai.ChatCompletion.create(
         model="gpt-4-0314",
-        max_tokens=1500,
+        max_tokens=30000,
         temperature=0.7,
-        messages = st.session_state.messages
+        messages = messages
     )
     return response.choices[0].message['content']
 
@@ -35,8 +35,12 @@ def call_openaiturbo(source):
 
 
 def main():
-    st.title("Chat Interface")
+    st.title("Pragmatic")
 
+    promp = st.text_area("Background information")
+    if st.button("Generate Answers"):
+        response = call_openai(promp)
+        st.write(response)
     conversation = st.empty()
 
     user_input = st.text_input("You:")
