3d66d4705515e68d33ba501a7f30152d71cfcc10
Jude Sajith <judesajith.aj@gmail.com>
2023-05-11 17:55:04 +0530

    sss

--- pages/demo.py
+++ pages/demo.py
@@ -39,7 +39,7 @@ def call_openaiturbo(inpt,source):
 
 
 
-marketcol , buyercol = st.columns(2)
+marketcol , buyercol = st.columns(2, gap="medium")
 
 if "response" not in st.session_state:
         st.session_state.response =""
