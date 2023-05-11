3125e51f49ac946cbbe22dd3559ec90cad0664b7
Jude Sajith <judesajith.aj@gmail.com>
2023-05-11 17:48:30 +0530

    ssss

--- pages/demo.py
+++ pages/demo.py
@@ -1,10 +1,11 @@
-import os
 import streamlit as st
+import os
 import openai 
 import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-st.set_page_config(page_title=None, page_icon=None, layout="centered", initial_sidebar_state="expanded", menu_items=None)
+
+
 def call_openai(source):
     # if "messages" not in st.session_state:
     messages=[{"role": "system", "content": f"You are the Buyer persona represents the following {source} given this information answer the following questions :\n What is your most important job responsibilities and activities? • What are the top 5 obstacles that would interfere with your career success? Where would you look for information about products like your company and other industry trends? • What role do you play in the purchase process, would they define the need, evaluate different solutions, make the final purchase decision or some combination of those roles? • What other roles in the company would likely be part of the buying process and decision-making? • Why wouldn’t you already have a product like ours? • Are there related products that this buyer’s team uses that would need to integrate with your product? • What concerns would you have that might keep them from making the purchase decision? \n the response should consist of the questions and the answers "}]
@@ -37,25 +38,42 @@ def call_openaiturbo(inpt,source):
 
 
 
-def main():
-    if "response" not in st.session_state:
+marketcol , buyercol = st.columns(2)
+
+if "response" not in st.session_state:
         st.session_state.response =""
 
-    st.title("Pragmatic Questionnare")
 
-    conversation = st.empty()
 
-    user_input = st.text_input("Buyer:", placeholder="Type start and click send to initiate the quetsionare")
 
-    if st.button("Send"):
-        bot_response = call_openaiturbo(user_input,str(st.session_state.response))
 
-        conversation.markdown(f'**You:** {user_input}')
-        conversation.info(f'**Bot:** {bot_response}')
+marketcol.title("Marketing Admin")
 
-if __name__ == "__main__":
-    main()
-    # if st.session_state.response is not None:
-    #     with st.expander("Sample persona answers"):
-    #         st.write(st.session_state.response)
+promp = marketcol.text_area("Background information of buyer")
+if marketcol.button("Stimulate"):
+    response = call_openai(promp)
+    # jsonstr = json.loads(response)
+    st.session_state.response = str(response)
+    # st.write(st.session_state.response)
 
+
+
+if st.session_state.response != "":
+    # email = st.text_input("Enter the email of the Buyer")
+    # send_email_button = st.button("Send email")
+    with marketcol.expander("Stimulted Persona"):
+        marketcol.write(st.session_state.response)
+
+
+
+buyercol.title("Pragmatic Questionnare")
+
+conversation = buyercol.empty()
+
+user_input = buyercol.text_input("Buyer:", placeholder="Type start and click send to initiate the quetsionare")
+
+if st.button("Send"):
+    bot_response = call_openaiturbo(user_input,str(st.session_state.response))
+
+    conversation.markdown(f'**You:** {user_input}')
+    conversation.info(f'**Bot:** {bot_response}')
