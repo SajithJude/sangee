b8e66fe5158b81c38c530e26189a5d7bbc9dc2b0
Jude Sajith <judesajith.aj@gmail.com>
2023-05-15 03:29:52 +0530

    ssss

--- pages/new_demo.py
+++ pages/new_demo.py
@@ -1,13 +1,13 @@
 import streamlit as st
 import openai
-import os 
+import os
+import json
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 def generate_persona(source):
-    
     response = openai.Completion.create(
-        model="text-davinci-003",
+        model="text-davinci-002",
         prompt=source,
         temperature=0.56,
         max_tokens=2066,
@@ -17,41 +17,40 @@ def generate_persona(source):
     )
     return response.choices[0].text
 
-# Title of your app
+# Load existing personas or initialize an empty dictionary
+try:
+    with open("db.json", "r") as f:
+        personas = json.load(f)
+except FileNotFoundError:
+    personas = {}
+
 st.title('AI-Powered Persona Generation Tool')
 
-# Radio button for B2B and B2C contexts
 context = st.radio('Please select the context:', ['B2B', 'B2C'])
 
-# Create three columns for the input fields
 col1, col2, col3, col4 = st.columns(4)
 
-# Input fields for demographic information
-# with col1:
 col1.subheader('Demographic Information')
 age = col1.number_input('Age', min_value=0, max_value=120, step=1)
 location = col1.text_input('Location')
 
-# Input fields for professional roles
-# with col2:
 col2.subheader('Professional Roles')
 role = col2.text_input('Role')
 industry = col2.text_input('Industry')
 company_size = col2.number_input('Company Size', min_value=1, step=1)
 
-# Input fields for company information
-# with col3:
 col3.subheader('Company Information')
 company_name = col3.text_input('Company Name')
 company_description = col3.text_area('Company Description', max_chars=250)
 
-# Input fields for product descriptions
 col4.subheader('Product Descriptions')
 product_name = col4.text_input('Product Name')
 product_description = col4.text_area('Product Description', max_chars=250)
 product_cost = col4.number_input('Product Cost', format="%f")
 
-# When user clicks this button, call your persona generation function with the provided inputs
+# Input field for nickname
+nickname = st.text_input('Nickname for the persona')
+
 if st.button('Generate Persona'):
 
     persona_prompt = f"The user is a {role} in the {industry} industry. They work for a company called {company_name} which has approximately {company_size} employees. "\
@@ -59,8 +58,14 @@ if st.button('Generate Persona'):
     f"They are responsible for the product named {product_name}, which costs approximately {product_cost} and can be described as follows: {product_description}. "\
     f"The user's demographic information is as follows: They are {age} years old. "\
     f"The context for persona generation is {context}."
-    
-    person_a = generate_persona(persona_prompt)
-    st.write(person_a)
 
+    # Generate persona and save it under the given nickname
+    persona = generate_persona(persona_prompt)
+    personas[nickname] = persona
+
+    # Save updated personas dictionary to db.json
+    with open("db.json", "w") as f:
+        json.dump(personas, f)
+
+    st.write(persona)
     st.success('Persona generated successfully!')
