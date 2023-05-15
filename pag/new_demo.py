a63afedca74f44b2f9decd5eae72f255f5490615
Jude Sajith <judesajith.aj@gmail.com>
2023-05-15 16:56:50 +0530

    ksksks

--- pag/new_demo.py
+++ pag/new_demo.py
@@ -170,12 +170,6 @@ elif persona_option == "chat":
 
 
 
-query_engine = index.as_query_engine()
-response = query_engine.query(f"Generate a Persona document for the following information: {persona_prompt}")
-but = st.button("jsnjs ")
-if but:
-    st.write(response)
-
 
 # else:
 #     section2.write("No personas available.")
